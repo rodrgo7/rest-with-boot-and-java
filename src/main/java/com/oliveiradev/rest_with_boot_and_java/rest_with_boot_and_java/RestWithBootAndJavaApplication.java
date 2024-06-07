7a669aa53e35217515b4b01d8477dd0b4881f04a
Rodrigo Oliveira <r.oliveira.95hotmail.com>
2024-06-07 14:30:20 -0300

    First commit

---
+++ src/main/java/com/oliveiradev/rest_with_boot_and_java/rest_with_boot_and_java/RestWithBootAndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.oliveiradev.rest_with_boot_and_java.rest_with_boot_and_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithBootAndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithBootAndJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliveiradev/rest_with_boot_and_java/rest_with_boot_and_java/RestWithBootAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveiradev.rest_with_boot_and_java.rest_with_boot_and_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithBootAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
