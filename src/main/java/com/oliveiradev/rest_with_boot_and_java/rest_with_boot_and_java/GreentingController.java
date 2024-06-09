a08a357aa86dc3080d1320b0576b86bad698a22c
Rodrigo Oliveira <r.oliveira.95@hotmail.com>
2024-06-09 19:36:31 -0300

    Change for MathController

--- src/main/java/com/oliveiradev/rest_with_boot_and_java/rest_with_boot_and_java/GreentingController.java
+++
@@ -1,8 +0,0 @@
-package com.oliveiradev.rest_with_boot_and_java.rest_with_boot_and_java;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController 
-public class GreentingController {
-    private static final String template = "Hello, %world";
-}
