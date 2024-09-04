d97d3cf885a48bcbff1cc363668d7034b79bd2d7
Rodrigo Oliveira <r.oliveira.95hotmail.com>
2024-09-04 10:46:42 -0300

    working with docker. note: place docker-compose outside the main folder

--- src/test/java/com/oliveiradev/tests/configs/TestConfigs.java
+++ src/test/java/com/oliveiradev/tests/configs/TestConfigs.java
@@ -1,7 +1,7 @@
 package com.oliveiradev.tests.configs;
 
 public class TestConfigs {
-    public static final int SERVER_PORT = 8888;
+    public static final int SERVER_PORT = 80;
 
     public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
     public static final String HEADER_PARAM_ORIGIN = "Origin";
